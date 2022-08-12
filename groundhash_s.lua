33e692baee8044c663e144189018cbf4b975502b
Case <90377400+CaseIRL@users.noreply.github.com>
2022-08-12 11:40:23 +0100

    Create groundhash_s.lua

---
+++ groundhash_s.lua
@@ -0,0 +1,7 @@
+----------------------------------
+--<!>-- BOII | DEVELOPMENT --<!>--
+----------------------------------
+
+RegisterCommand('checkgroundhash', function(source)
+    TriggerClientEvent('groundhashtool:checkhash', source)
+end)
