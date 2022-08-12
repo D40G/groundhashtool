02970385af847b20730b5af49f98d593f8ffd8b2
Case <90377400+CaseIRL@users.noreply.github.com>
2022-08-12 11:41:18 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+----------------------------------
+--<!>-- BOII | DEVELOPMENT --<!>--
+----------------------------------
+
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'CASE#1993'
+
+description 'BOII | Development - Simple Ground Hash Check'
+
+version '1.0.0'
+
+client_scripts {
+    'groundhash_c.lua'
+}
+
+server_scripts{
+    'groundhash_s.lua'
+}
